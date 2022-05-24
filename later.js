0a8dedd55015139161560a83918ea59514e32b62
Laura Lee Gwinn <ells.gwinn@gmail.com>
2022-05-24 11:13:22 -0400

    removed extraneous file later.js

--- later.js
+++
@@ -1,9 +0,0 @@
-useEffect( async() => {
-    // get user's favorite artists
-    async function fetchArtists(userId) {
-        const artistsRes = await ArtistTrackerApi.getUsersArtists(userId);
-        setArtists(artistsRes);
-        console.log(artistsRes);
-    }
-    fetchArtists(currUser.id)
-}, [])
