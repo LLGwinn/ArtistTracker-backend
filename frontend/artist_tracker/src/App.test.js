f27fdffbaba8fd274489131eb11f63e300142f7b
Laura Lee Gwinn <ells.gwinn@gmail.com>
2022-05-24 11:45:20 -0400

    relocated frontend files to separate git

--- frontend/artist_tracker/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import App from './App';
-
-it ('renders without crashing', function() {
-  render(<MemoryRouter>
-            <App />
-        </MemoryRouter>);
-});
